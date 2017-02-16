b1fe9b59c8e7cb501c82ace357937c8c347d6363
kimjohnconcha <kimjohnconcha@icloud.com>
2017-02-16 22:54:02 +0800

    DB Init
    Signed-off-by: kimjohnconcha <kimjohnconcha@icloud.com>

--- Expressway/src/EM/EmployeeEM.java
+++ Expressway/src/EM/EmployeeEM.java
@@ -6,9 +6,11 @@
 package EM;
 
 import Model.Employee;
+import Model.Position;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
@@ -25,23 +27,121 @@ public class EmployeeEM {
         String query = "";
         PreparedStatement ps = null;
         ResultSet rs = null;
-        ArrayList<Employee> employee = new ArrayList<>();
+        ArrayList<Employee> employees = new ArrayList<>();
         
-        // Some Magic here
-        
-        
-        return employee;
+        try {
+            ps = con.prepareStatement(query);
+            rs = ps.executeQuery();
+            
+            if(rs.next())
+            {
+                do
+                {
+                    Employee employee = new Employee();
+                    employee.setEmployeeCode(rs.getString("employee_code"));
+                    employee.setLastname(rs.getString("last_name"));
+                    employee.setFirstname(rs.getString("first_name"));
+                    employee.setMiddlename(rs.getString("middle_name"));
+                    
+                    Position position = new Position();
+                    position.setPositionID(rs.getInt("position_id"));
+                    position.setPositionCode(rs.getString("position_code"));
+                    position.setPositionName(rs.getString("position_name"));
+                    
+                    employee.setPosition(position);
+                    employees.add(employee);
+                    
+                } while(rs.next());
+            }
+
+        } catch(SQLException ex) {
+            System.out.println("Error " + ex.getMessage());
+        } finally {
+            if(ps != null)
+            {
+                try {
+                    ps.close();
+                } catch(SQLException ex) {
+                    System.out.println("Error " + ex.getMessage());
+                }
+            }
+            if(rs != null)
+            {
+               try {
+                    rs.close();
+                } catch(SQLException ex) {
+                    System.out.println("Error " + ex.getMessage());
+                } 
+            }
+            
+            return employees;
+        }
     }
+    
     
     public ArrayList<Employee> get(String criterion) {
         String query = "";
         PreparedStatement ps = null;
         ResultSet rs = null;
-        ArrayList<Employee> employee = new ArrayList<>();
+        ArrayList<Employee> employees = new ArrayList<>();
         
-        // Some Magic here
-        
-        return employee;
+        try {
+            ps = con.prepareStatement(query);
+            
+            int i = 1;
+            ps.setString(i++, criterion + "%");
+            ps.setString(i++, "%" + criterion + "%");
+            ps.setString(i++, "%" + criterion + "%");
+            ps.setString(i++, "%" + criterion + "%");
+            ps.setString(i++, "%" + criterion + "%");
+            ps.setString(i++, "%" + criterion + "%");
+            
+            rs = ps.executeQuery();
+            
+            if(rs.next())
+            {
+                do
+                {
+                    Employee employee = new Employee();
+                    employee.setEmployeeCode(rs.getString("employee_code"));
+                    employee.setLastname(rs.getString("last_name"));
+                    employee.setFirstname(rs.getString("first_name"));
+                    employee.setMiddlename(rs.getString("middle_name"));
+                    
+                    Position position = new Position();
+                    position.setPositionID(rs.getInt("position_id"));
+                    position.setPositionCode(rs.getString("position_code"));
+                    position.setPositionName(rs.getString("position_name"));
+                    
+                    employee.setPosition(position);
+                    employees.add(employee);
+                    
+                } while(rs.next());
+            }
+
+        } catch(SQLException ex) {
+            System.out.println("Error " + ex.getMessage());
+        } finally {
+            
+            if(ps != null)
+            {
+                try {
+                    ps.close();
+                } catch(SQLException ex) {
+                    System.out.println("Error " + ex.getMessage());
+                }
+            }
+            if(rs != null)
+            {
+               try {
+                    rs.close();
+                } catch(SQLException ex) {
+                    System.out.println("Error " + ex.getMessage());
+                } 
+            }
+            
+            return employees;
+        }
     }
     
     public int persist(Employee employee) {
@@ -50,9 +150,34 @@ public class EmployeeEM {
         PreparedStatement ps = null;
         int row = 0;
         
-        // Some magic here
-        
-        return row;
+        try {
+            ps = con.prepareStatement(query);
+            
+            int i = 1;
+            ps.setString(i++, employee.getEmployeeCode());
+            ps.setString(i++, employee.getLastname());
+            ps.setString(i++, employee.getFirstname());
+            ps.setString(i++, employee.getMiddlename());
+            ps.setInt(i++, employee.getPosition().getPositionID());
+            
+            row = ps.executeUpdate();
+
+        } catch(SQLException ex) {
+            System.out.println("Error " + ex.getMessage());
+        } finally {
+            
+            if(ps != null)
+            {
+                try{
+                    ps.close();
+                } catch (SQLException ex) {
+                    System.out.println("Error " + ex.getMessage());
+                }
+            }
+         
+            return row;
+        }
+
     }
     
     public int delete(String employeeCode) {
@@ -60,20 +185,84 @@ public class EmployeeEM {
         PreparedStatement ps = null;
         int row = 0;
         
-        // Some magic here
-        
-        return row;
+        try {
+            ps = con.prepareStatement(query);
+            
+            int i = 1;
+            ps.setString(i++, employeeCode);
+            
+            row = ps.executeUpdate();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error " + ex.getMessage());
+        } finally {
+            
+            if(ps != null)
+            {
+                try{
+                    ps.close();
+                } catch (SQLException ex) {
+                    System.out.println("Error " + ex.getMessage());
+                }
+            }
+            
+            return row;
+        }
     }
     
     public Employee getEmployee(String employeeCode) {
         String query = "";
+        
         PreparedStatement ps = null;
-        int row = 0;
+        ResultSet rs = null;
         Employee employee = new Employee();
         
-        // Some magic here
+        try {
+            ps = con.prepareStatement(query);
+            ps.setString(1, employeeCode);
+            
+            rs = ps.executeQuery();
+            
+            if(rs.next()) {
+                employee.setEmployeeID(rs.getInt("employee_id"));
+                employee.setEmployeeCode(rs.getString("employee_code"));
+                employee.setLastname(rs.getString("last_name"));
+                employee.setFirstname(rs.getString("first_name"));
+                employee.setMiddlename(rs.getString("middle_name"));
+
+                Position position = new Position();
+                position.setPositionID(rs.getInt("position_id"));
+                position.setPositionCode(rs.getString("position_code"));
+                position.setPositionName(rs.getString("position_name"));
+
+                employee.setPosition(position);
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("Error " + ex.getMessage());
+        } finally {
+            
+            if(ps != null)
+            {
+                try {
+                    ps.close();
+                } catch(SQLException ex) {
+                    System.out.println("Error " + ex.getMessage());
+                }
+            }
+            if(rs != null)
+            {
+               try {
+                    rs.close();
+                } catch(SQLException ex) {
+                    System.out.println("Error " + ex.getMessage());
+                } 
+            }
+            
+            return employee;
+        }
         
-        return employee;
+        
     }
     
     public int update(Employee employee, String oldCode) {
@@ -81,10 +270,40 @@ public class EmployeeEM {
         PreparedStatement ps = null;
         int row = 0;
         
-        // Some magic here
-        
-        return row;
+        try {
+            ps = con.prepareStatement(query);
+            
+            int i = 1;
+            ps.setString(i++, employee.getEmployeeCode());
+            ps.setString(i++, employee.getLastname());
+            ps.setString(i++, employee.getFirstname());
+            ps.setString(i++, employee.getMiddlename());
+            ps.setInt(i++, employee.getPosition().getPositionID());
+            ps.setString(i++, oldCode);
+            
+            row = ps.executeUpdate();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error " + ex.getMessage());
+        } finally {
+            
+            if(ps != null)
+            {
+                try {
+                    ps.close();
+                } catch(SQLException ex) {
+                    System.out.println("Error " + ex.getMessage());
+                }
+            }
+            
+            return row;
+        }
+
     }
+    
+    
+    
+    
     
     
 }
